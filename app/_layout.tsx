0398bb3136743e0b8ec489add0cd5817a3db2d08
Vadim Savin <savinvadim1312@gmail.com>
2024-07-26 17:16:13 +0300

    Initial commit
    Generated by create-expo-stack 2.11.14

---
+++ app/_layout.tsx
@@ -0,0 +1,7 @@
+import '../global.css';
+
+import { Stack } from 'expo-router';
+
+export default function Layout() {
+  return <Stack />;
+}
---
+++ components/Button.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from 'react';
+import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+
+type ButtonProps = {
+  title: string;
+} & TouchableOpacityProps;
+
+export const Button = forwardRef<TouchableOpacity, ButtonProps>(
+  ({ title, ...touchableProps }, ref) => {
+    return (
+      <TouchableOpacity
+        ref={ref}
+        {...touchableProps}
+        className={`${styles.button} ${touchableProps.className}`}>
+        <Text className={styles.buttonText}>{title}</Text>
+      </TouchableOpacity>
+    );
+  }
+);
+
+const styles = {
+  button: 'items-center bg-indigo-500 rounded-[28px] shadow-md p-4',
+  buttonText: 'text-white text-lg font-semibold text-center',
+};
