198fa1901f917d4dbb35989ed73fae66df7e3dfa
Vadim Savin <savinvadim1312@gmail.com>
2024-07-26 17:22:46 +0300

    initial cleanup

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,12 @@
-import { Stack, Link } from 'expo-router';
-
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
+import { Stack } from 'expo-router';
+import { Text } from 'react-native';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
+
+      <Text className="text-2xl font-bold color-blue-800">Hello world</Text>
     </>
   );
 }
