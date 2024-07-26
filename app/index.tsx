04a06df2083c4390b733ff2419c5697442f4a527
Vadim Savin <savinvadim1312@gmail.com>
2024-07-26 20:18:32 +0300

    fix supabase web and web styles

--- app/index.tsx
+++ app/index.tsx
@@ -24,8 +24,8 @@ export default function Home() {
   };
 
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
+    <View className="mx-auto w-full max-w-xl">
+      <Stack.Screen options={{ title: 'Translate' }} />
 
       {/* Language selector row */}
       <View className="flex-row justify-around p-5">
@@ -76,6 +76,6 @@ export default function Home() {
           </View>
         </View>
       )}
-    </>
+    </View>
   );
 }
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,12 +1,13 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: AsyncStorage,
+    ...(Platform.OS !== "web" ? { storage: AsyncStorage } : {}),
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
